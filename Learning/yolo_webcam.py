68974b00c3afbe67312741c82a54a877e240f431
hetbhalani <bhalanihet2006@gmail.com>
2024-11-05 21:03:13 +0530

    webcam

--- Learning/yolo_webcam.py
+++ Learning/yolo_webcam.py
@@ -6,9 +6,17 @@ cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
 
-model = YOLO("../")
+model = YOLO("./Yolo-Weights/yolov8n.pt")
 
 while True:
     success, img = cap.read()
+    results = model(img,stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+            cv2.rectangle(img,(x1,y1), (x2,y2),(0,0,255),3)
+            print(x1,y1,x2,y2)
     cv2.imshow("image",img)
     cv2.waitKey(1)
