05b4f22dbfacc7f3665bc636ce9fc4644f36afbe
hetbhalani <bhalanihet2006@gmail.com>
2024-11-05 20:46:40 +0530

    webcam done

--- Learning/yolo_webcam.py
+++ Learning/yolo_webcam.py
@@ -3,7 +3,10 @@ import cv2
 import cvzone
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
+cap.set(3,1280)
+cap.set(4,640)
 
-while
+while True:
+    success, img = cap.read()
+    cv2.imshow("image",img)
+    cv2.waitKey(1)
