9b1b13075a6fc356b6838bd3547c5d5dc3baa715
hetbhalani <bhalanihet2006@gmail.com>
2024-11-05 20:52:33 +0530

    yolo

--- Learning/running_yolo.py
+++ Learning/running_yolo.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO('./Yolo-Weights/yolov8l.pt')
-results = model("./imgs/sch_bus.jpg", show=True)
+model = YOLO('./Yolo-Weights/yolov8n.pt')
+results = model("../imgs/sch_bus.jpg", show=True)
 cv2.waitKey(0)
