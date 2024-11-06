2391326ea68abfa83d8ed7c4de9e7f6099b4b96f
hetbhalani <bhalanihet2006@gmail.com>
2024-11-06 10:57:33 +0530

    obj on webcam

--- Learning/yolo_webcam.py
+++ Learning/yolo_webcam.py
@@ -9,6 +9,18 @@ cap.set(4, 480)
 
 model = YOLO("./Yolo-Weights/yolov8n.pt")
 
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
 while True:
     success, img = cap.read()
     results = model(img,stream=True)
@@ -24,7 +36,6 @@ while True:
             cvzone.cornerRect(img,(x1,y1,w,h))
             
             conf = math.ceil(box.conf[0]*100)/100
-            print(conf)
             cvzone.putTextRect(img,f'{conf}',(max(0,x1),max(35,y1)))
 
     cv2.imshow("image",img)
