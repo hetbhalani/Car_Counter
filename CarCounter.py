1d16b21de6b43f39e0c4163d93f616887665a492
hetbhalani <bhalanihet2006@gmail.com>
2024-11-07 16:27:49 +0530

    detecting mask

--- CarCounter.py
+++ CarCounter.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 import math
+from sort import *
 
 cap = cv2.VideoCapture("./imgs/cars.mp4")
 
@@ -27,7 +28,7 @@ while True:
     success, img = cap.read()
     imgRegion = cv2.bitwise_and(img,mask)
     
-    results = model(img,stream=True)
+    results = model(imgRegion,stream=True)
     for r in results:
         boxes = r.boxes
         for box in boxes:
