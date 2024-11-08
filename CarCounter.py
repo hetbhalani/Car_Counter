e484a6efae8a628a8fa8089508d41661aa09e037
hetbhalani <bhalanihet2006@gmail.com>
2024-11-08 15:04:51 +0530

    count cars

--- CarCounter.py
+++ CarCounter.py
@@ -40,7 +40,6 @@ while True:
         for box in boxes:
             #bounding box
             
-            #this is using cv2
             x1,y1,x2,y2 = box.xyxy[0]
             x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
             # cv2.rectangle(img,(x1,y1), (x2,y2),(0,255,0),3)
